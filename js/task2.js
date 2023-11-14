713f2b39eb353baae5cd29df4c10f89511e9384b
YuriySoroka <40ayuriy@gmail.com>
2023-11-14 20:06:02 +0200

    correction

--- js/task2.js
+++ js/task2.js
@@ -1,14 +1,13 @@
 function getTopCities(CSV) {
-    let topCities = {};
     /* array of strings by carriage */
-    CSV.split("\n")
+    let topCities = CSV.split("\n")
         /* split the term into arrays using the "," separator, take only 4 elements */
         .map(elem => elem.split(",", 4))
         /* filtering out empty lines and lines with "#" */
         .filter(elem => elem.every(e => !e.includes("#") && e.length != 0))
         /* recording values in properties */
         .map(e => {
-            let o = [];
+            let o = {};
             [o.x, o.y, o.name, o.population] = e;
             return o;
         })
@@ -17,16 +16,18 @@ function getTopCities(CSV) {
         /* select the first 10 cities */
         .slice(0, 10)
         /* filling the object with data of the type: Kyiv: {x,y,population, rating} */
-        .map((elem, i) => {
-            elem.rating = ++i;
-            topCities[elem.name] = elem;
-            delete topCities[elem.name].name;
-        })
+        .reduce((prefabObj = {}, elem, i) => {
+            prefabObj[elem.name] = elem;
+            prefabObj[elem.name].rating = ++i;
+            delete elem.name;
+           return prefabObj;
+        },undefined);
 
     /* replacement in the line of city names with a line of information about this city */
     return (str) => {
+        console.log(topCities);
         Object.keys(topCities).map(city => {
-            let reg= new RegExp( city , 'gi');
+            let reg = new RegExp(city, 'gi');
             str = str.replace(reg, `${city} (${topCities[city].rating} місце в ТОП-10 найбільших міст України, населення ${topCities[city].population} людей)`);
         })
         return str;
